2839924a0f3a13d4543317d7c8341a94ff2570bf
AndreySVRF <andreysmagin14@gmail.com>
2020-09-16 22:49:52 +0400

    Закончил 5 урок

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,18 +1,22 @@
-import React from "react";
-
-import classNames from "classnames";
+import React from 'react';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
 
 const Button = ({ className, outline, onClick, children }) => {
   return (
     <button
-      className={classNames("button", className, {
-        "buttton--outline": outline,
+      className={classNames('button', className, {
+        'buttton--outline': outline,
       })}
       onClick={onClick}
     >
       {children}
     </button>
   );
+};
+
+Button.propTypes = {
+  onClick: PropTypes.func,
 };
 
 export default Button;
